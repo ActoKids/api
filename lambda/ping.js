c9ef0bf9345b8d758189535513d32ff192ec9a3f
Jonathan Adler <jradler@gmail.com>
2019-02-15 14:35:01 -0800

    add comments for clarity

--- lambda/ping.js
+++ lambda/ping.js
@@ -1,11 +1,17 @@
 
+/*
+    handler is the function that AWS Lambda invokes when triggered by API Gateway. handler runs in response to 
+    every trigger, but code outside of handler (if any) runs only when the execution environment is created. 
+*/
 exports.handler = async (event) => {
     console.log(`Logging ${event.key1}`)
     let date = new Date();
+    //Content to be displayed to the user upon sucessfull call
     let bodyContent = {
         pingMessage : "API is working!",
         todaysDate : date,
     }
+    //Set Access-Control-Allow-Origin to wildcard to ensure API can be invoked by client-side code
     const response = {
         statusCode: 200,
         headers : {
