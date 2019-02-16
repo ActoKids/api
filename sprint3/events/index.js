87648daa351d36371e97d28a20342e9fb86a5f5c
T-travis <ttravis1111@gmail.com>
2019-02-16 10:28:18 -0800

    added logs to lambda function for sprint 3

--- sprint3/events/index.js
+++ sprint3/events/index.js
@@ -1,19 +1,27 @@
-const AWS = require('aws-sdk'),
-	documentClient = new AWS.DynamoDB.DocumentClient({ region: 'us-west-2'}); 
+// aws-sdk is needed for its libraries that allow to access other aws services 
+const AWS = require('aws-sdk');
+// the DynamoDB.DocumentClient class allows access to Dynamodb directly, 
+const documentClient = new AWS.DynamoDB.DocumentClient({ region: 'us-west-2'}); 
 	
-AWS.config.update({ region: 'us-west-2'});
-
+// this package is needed to validate parameters using that are uuids
 const validate = require('uuid-validate');
 
 // lambda function that deletes an Events db item by a given primary key
+// it will need to handle PUT requests and GET in later sprints
 exports.handler = async (event, context) => {
+    // log for when lambda function starts
+    console.log("Events lambda function started...");
     
-    // this stores the input given by the caller
+    // this stores the input given by the caller, a UUID for a specific item in the Events table
     const ID = event.pathParameters.eventID;
+    
+    // log for recording the given url parameter given
+    console.log("Path parameter, eventID: ", ID);
+    
     // this stores the http method used by the caller
     const httpMethod = event.httpMethod;
     
-    // validate input parameter is uuid version 1
+    // validate input parameter is uuid version 1, when fails a 400 response must be given
     if(!validate(ID, 1)) {
     	var response = {
 		    statusCode: 400,
@@ -29,33 +37,38 @@ exports.handler = async (event, context) => {
    // only using DELETE now but will need the others soon
    if (httpMethod === 'DELETE') {
    	
-		// params contains the table name and primary key to delete an item given the id 
+		// params contains the table name and primary key to delete an item given url parameter
 	    var params = {
-	        TableName : "Events",
+	        TableName : "vents",
 	        Key : {
 				"eventID" : ID
 			},
-			"ConditionExpression": "attribute_not_exists(Replies)",
+			// needed to return deleted item or empty object if nothing found to delete by given ID
 	        "ReturnValues": "ALL_OLD"
 		};
 		
 		let code;
 		try {
-			// delete item
+			// delete method is needed to delete items from a Dynamodb table
 		    const data = await documentClient.delete(params).promise();
 		    console.log("DATA: ", data);
 		    
-		    // check if nothing was returned to cause a 404 response, item not found in db
-		    if(Object.entries(data).length === 0) {
+		    // check if nothing was returned to cause a 404 response, item not found in db 
+		    if(Object.entries(data).length === 0 && data.constructor === Object) {
 		    	code = 404;
+		    	// log needed for when a parameter eventID is not found in the db
+		    	console.log("eventID not found in Table.")
 		    }
 		    else {
 		    	code = 200;
+		    	// log needed to confirm successful deletion of item in db
+		    	console.log("Item successfully deleted.")
 		    }
 			 
 		} catch(err) {
 			code = 500;
-			console.log("Error: ", err);
+			// error log needed to record internal server errors
+			console.error("Internal Server Error: ", err);
 		}
 		
 		// if you want a mesage for humans add body after headers
@@ -69,12 +82,13 @@ exports.handler = async (event, context) => {
 	    return response;
 		
    } else if (httpMethod === 'GET') {
-      // not used yet
+      // not used in this sprint
    } else if (httpMethod === 'PUT') {
-      // not used yet
+      // not used in this sprint
    }
  
-   
+   // log for when lambda function ends
+    console.log("Events lambda function ended.");
 };
 
 
