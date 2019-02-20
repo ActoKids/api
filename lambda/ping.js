84d49fa58128ee14a69610f1efc6905561d400a5
Jonathan Adler <jradler@gmail.com>
2019-02-20 13:12:01 -0800

    add logs, add request type to response, update comments

--- lambda/ping.js
+++ lambda/ping.js
@@ -1,15 +1,17 @@
-
 /*
     handler is the function that AWS Lambda invokes when triggered by API Gateway. handler runs in response to 
     every trigger, but code outside of handler (if any) runs only when the execution environment is created. 
 */
 exports.handler = async (event) => {
-    console.log(`Logging ${event.key1}`)
-    let date = new Date();
-    //Content to be displayed to the user upon sucessfull call
+    console.log("Received ping request...");
+    /*
+    Content to be displayed to the user upon sucessfull call.  Since API is set up to handle ANY request, the 
+    request type is included in the response for testing purposes.
+    */
     let bodyContent = {
-        pingMessage : "API is working!",
-        todaysDate : date,
+        pingMessage : 'API is working',
+        todaysDate : new Date(),
+        requestType: event.httpMethod
     }
     //Set Access-Control-Allow-Origin to wildcard to ensure API can be invoked by client-side code
     const response = {
@@ -20,4 +22,5 @@ exports.handler = async (event) => {
         body: JSON.stringify(bodyContent)
     };
     return response;
+    console.log("Returned ping response");
 };
