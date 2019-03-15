3f362630a100d1127464441f2b15e190a9bc8bb1
T-travis <ttravis1111@gmail.com>
2019-03-15 09:20:10 -0700

    updated delete lambda for us-west-2, updated Table name to table in PROD, and removed http Delete test that was not needed

--- lambda/events_id/delete/index.js
+++ lambda/events_id/delete/index.js
@@ -2,7 +2,7 @@
 const AWS = require('aws-sdk');
 // the DynamoDB.DocumentClient class allows access to Dynamodb directly 
 // region must be us-west-2 for Oregon/Prod
-const documentClient = new AWS.DynamoDB.DocumentClient({ region: 'us-east-2'}); 
+const documentClient = new AWS.DynamoDB.DocumentClient({ region: 'us-west-2'}); 
 	
 // this package is needed to validate parameters that are uuids
 const validate = require('uuid-validate');
@@ -44,54 +44,46 @@ exports.handler = async (event, context, callback) => {
 	
     }
     else {
-	
-	   // determine what http request is made to handle it appropriately 
-	   // *** only using DELETE now but will need the others soon
-	   if (httpMethod === 'DELETE') {
 	   	
-			// params contains the table name and primary key to delete an item with the given url parameter
-		    var params = {
-		        TableName : "ak-api-dynamo",
-		        Key : {
-					"event_id" : ID
-				},
-				// needed to return deleted item or empty object if nothing found to delete by given ID
-		        "ReturnValues": "ALL_OLD"
-			};
+		// params contains the table name and primary key to delete an item with the given url parameter
+		var params = {
+			TableName : "ak-prod-events-dynamo",
+			Key : {
+				"event_id" : ID
+			},
+		    // needed to return deleted item or empty object if nothing found to delete by given ID
+		    "ReturnValues": "ALL_OLD"
+		};
 			
 	
-			try {
-				// delete method is needed to delete items from a Dynamodb table
-			    const data = await documentClient.delete(params).promise();
+		try {
+			// delete method is needed to delete items from a Dynamodb table
+			const data = await documentClient.delete(params).promise();
 			    
-			    // check if nothing was returned to cause a 404 response, item not found in db 
-			    if(Object.entries(data).length === 0 && data.constructor === Object) {
-			    	// log needed for when a parameter eventID is not found in the db
-			    	console.log(`event_id ${ID} not found in Table.`);
-			    	response.statusCode = 404;
-			    	callback(JSON.stringify(response), null);
-			    }
-			    else {
-			    	// log needed to confirm successful deletion of item in db
-			    	console.log("Item successfully deleted.");
-			    	// response.statusCode = 200; already set to 200
-			    	callback(null, "Item Deleted");
-			    }
-				 
-			} catch(err) {
-				// error log needed to record internal server errors
-				console.error("Internal Server Error: ", err);
-				response.statusCode = 500;
-				callback(JSON.stringify(response), null);
+			// check if nothing was returned to cause a 404 response, item not found in db 
+			if(Object.entries(data).length === 0 && data.constructor === Object) {
+				// log needed for when a parameter eventID is not found in the db
+			    console.log(`event_id ${ID} not found in Table.`);
+			    response.statusCode = 404;
+			    callback(JSON.stringify(response), null);
 			}
-			
-	   }
+			else {
+				// log needed to confirm successful deletion of item in db
+			    console.log("Item successfully deleted.");
+			    // response.statusCode = 200; already set to 200
+			    callback(null, "Item Deleted");
+			}
+				 
+		} catch(err) {
+			// error log needed to record internal server errors
+			console.error("Internal Server Error: ", err);
+			response.statusCode = 500;
+			callback(JSON.stringify(response), null);
+		}
 	   
     }
  
    // log for when lambda function ends
     console.log("ak-api-travis-lambda lambda function ended.");
 };
-
-
 
